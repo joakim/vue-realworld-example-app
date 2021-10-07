e49857ac89cbeb6b6eb3c4c768762d70e0502da5
Joakim <joakim@users.noreply.github.com>
2021-10-07 19:55:25 +0200

    Update main.kesh.lua

--- src/main.kesh.lua
+++ src/main.kesh.lua
@@ -1,13 +1,13 @@
-#import (Vue) from "vue"
-#import (App) from "./App.vue"
-#import (router) from "./router"
-#import (store) from "./store"
-#import "./registerServiceWorker"
+import (Vue): "vue"
+import (App): "./App.vue"
+import (router): "./router"
+import (store): "./store"
+import "./registerServiceWorker"
 
-#import [ CHECK-AUTH ] from "./store/actions.type"
-#import (ApiService) from "./common/api.service"
-#import (DateFilter) from "./common/date.filter"
-#import (ErrorFilter) from "./common/error.filter"
+import [ CHECK-AUTH ]: "./store/actions.type"
+import (ApiService): "./common/api.service"
+import (DateFilter): "./common/date.filter"
+import (ErrorFilter): "./common/error.filter"
 
 set Vue.config.productionTip: false
 Vue.filter("date", DateFilter)
