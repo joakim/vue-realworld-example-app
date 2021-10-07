8547e066e20e0a7e47391498871209e93ef11d60
Joakim <joakim@users.noreply.github.com>
2021-10-07 20:12:34 +0200

    Update api.service.js

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -1,106 +1,89 @@
-import Vue from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import JwtService from "@/common/jwt.service";
-import { API_URL } from "@/common/config";
+import (Vue): "vue"
+import (axios): "axios"
+import (VueAxios): "vue-axios"
+import (JwtService): "@/common/jwt.service"
+import (default as [API-URL]): "@/common/config"
 
-const ApiService = {
-  init() {
-    Vue.use(VueAxios, axios);
-    Vue.axios.defaults.baseURL = API_URL;
-  },
+ApiService: [
+  init: () *->
+    Vue.use(VueAxios, axios)
+    set Vue.axios.defaults.baseURL: API-URL
 
-  setHeader() {
-    Vue.axios.defaults.headers.common[
+  setHeader: () *->
+    set Vue.axios.defaults.headers.common.{
       "Authorization"
-    ] = `Token ${JwtService.getToken()}`;
-  },
+    }: "Token { JwtService.getToken() }"
 
-  query(resource, params) {
-    return Vue.axios.get(resource, params).catch(error => {
-      throw new Error(`[RWV] ApiService ${error}`);
-    });
-  },
+  query: (resource, params) ->
+    Vue.axios.get(resource, params)
+      .catch (error) *->
+        throw Error "[RWV] ApiService { error }"
 
-  get(resource, slug = "") {
-    return Vue.axios.get(`${resource}/${slug}`).catch(error => {
-      throw new Error(`[RWV] ApiService ${error}`);
-    });
-  },
+  get: (resource, slug ? "") ->
+    Vue.axios.get("{ resource }/{ slug }")
+      .catch (error) *->
+        throw Error "[RWV] ApiService { error }"
 
-  post(resource, params) {
-    return Vue.axios.post(`${resource}`, params);
-  },
+  post: (resource, params) ->
+    Vue.axios.post("{ resource }", params)
 
-  update(resource, slug, params) {
-    return Vue.axios.put(`${resource}/${slug}`, params);
-  },
+  update: (resource, slug, params) ->
+    Vue.axios.put("{ resource }/{ slug }", params)
 
-  put(resource, params) {
-    return Vue.axios.put(`${resource}`, params);
-  },
+  put: (resource, params) ->
+    Vue.axios.put("{ resource }", params)
 
-  delete(resource) {
-    return Vue.axios.delete(resource).catch(error => {
-      throw new Error(`[RWV] ApiService ${error}`);
-    });
-  }
-};
+  delete: (resource) ->
+    Vue.axios.delete(resource)
+      .catch (error) *->
+        throw Error "[RWV] ApiService { error }"
+]
 
-export default ApiService;
+export default: ApiService
 
-export const TagsService = {
-  get() {
-    return ApiService.get("tags");
-  }
-};
+export TagsService: [
+  get: () -> ApiService.get("tags")
+]
 
-export const ArticlesService = {
-  query(type, params) {
-    return ApiService.query("articles" + (type === "feed" ? "/feed" : ""), {
-      params: params
-    });
-  },
-  get(slug) {
-    return ApiService.get("articles", slug);
-  },
-  create(params) {
-    return ApiService.post("articles", { article: params });
-  },
-  update(slug, params) {
-    return ApiService.update("articles", slug, { article: params });
-  },
-  destroy(slug) {
-    return ApiService.delete(`articles/${slug}`);
-  }
-};
+export ArticlesService: [
+  query: (type, params) ->
+    ApiService.query("articles{ '/feed' if type = 'feed' else '' }", [params])
 
-export const CommentsService = {
-  get(slug) {
-    if (typeof slug !== "string") {
-      throw new Error(
+  get: (slug) ->
+    ApiService.get("articles", slug)
+
+  create: (params) ->
+    ApiService.post("articles", [article: params])
+
+  update: (slug, params) ->
+    ApiService.update("articles", slug, [article: params])
+
+  destroy: (slug) ->
+   ApiService.delete("articles/{ slug }")
+]
+
+export CommentsService: [
+  get: (slug) ->
+    if slug is #string
+      throw Error(
         "[RWV] CommentsService.get() article slug required to fetch comments"
-      );
-    }
-    return ApiService.get("articles", `${slug}/comments`);
-  },
+      )
 
-  post(slug, payload) {
-    return ApiService.post(`articles/${slug}/comments`, {
-      comment: { body: payload }
-    });
-  },
+    ApiService.get("articles", "{ slug }/comments")
 
-  destroy(slug, commentId) {
-    return ApiService.delete(`articles/${slug}/comments/${commentId}`);
-  }
-};
+  post: (slug, payload) ->
+    ApiService.post("articles/{ slug }/comments", [
+      comment: [body: payload]
+    ])
 
-export const FavoriteService = {
-  add(slug) {
-    return ApiService.post(`articles/${slug}/favorite`);
-  },
-  remove(slug) {
-    return ApiService.delete(`articles/${slug}/favorite`);
-  }
-};
+  destroy: (slug, commentId) ->
+    ApiService.delete("articles/{ slug }/comments/{ commentId }"
+]
+
+export FavoriteService: [
+  add: (slug) ->
+    ApiService.post("articles/{ slug }/favorite")
+
+  remove: (slug) ->
+    ApiService.delete("articles/{ slug }/favorite")
+]
